43573d2fdee74532bcf879ac2e26611e1d172e86
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-27 19:33:48 -0500

    Configuración de variables de entorno en microservicio de producto

--- 02-products-app/products-ms/src/main.ts
+++ 02-products-app/products-ms/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true
+    })
+  );
+
+  await app.listen(envs.port);
+  console.log(`Aplicación ejecutandose en el puerto ${envs.port}`);
+  
 }
 bootstrap();
