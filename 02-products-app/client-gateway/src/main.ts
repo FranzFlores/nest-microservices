436c53cd4042a38ac325d9ae9af7cee52278563c
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-28 11:11:31 -0500

    Conectar los productos de client gateway al microservicio de productos

--- 02-products-app/client-gateway/src/main.ts
+++ 02-products-app/client-gateway/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { RpcCustomeExceptionFilter } from './common/exceptions';
+
 
 async function bootstrap() {
+  const logger = new Logger('Main Gateway');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true
+    })
+  );
+
+  app.useGlobalFilters(
+    new RpcCustomeExceptionFilter()
+  );
+
+  await app.listen(envs.port);
+
+  logger.log(`Gateway corriendo en el puerto ${envs.port}`);
 }
 bootstrap();
---
+++ 02-products-app/client-gateway/src/products/products.controller.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+
+import { PaginationDto } from 'src/common/dto';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('/products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE)
+    private readonly productsClient: ClientProxy
+  ) { }
+
+  @Post('/create')
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send({ cmd: 'create_product' }, createProductDto)
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
+  }
+
+  @Get('/all')
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send({ cmd: 'all_products' }, paginationDto)
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
+  }
+
+  @Get('/:id')
+  async findOneProducts(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'one_product' }, { id })
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
+  }
+
+  @Patch('/update/:id')
+  updateProduct(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsClient.send({ cmd: 'update_product' }, { id, ...updateProductDto })
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
+  }
+
+  @Delete('/delete/:id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsClient.send({ cmd: 'delete_product' }, { id })
+      .pipe(
+        catchError(err => { throw new RpcException(err) })
+      );
+  }
+}
