6cbe4576c68b35511a51572932f647cf382f721a
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-27 20:53:21 -0500

    Transformar el servidor HTTP a un microservicio

--- 02-products-app/products-ms/src/main.ts
+++ 02-products-app/products-ms/src/main.ts
@@ -1,15 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+import { AppModule } from './app.module';
 import { envs } from './config/envs';
+
 
 async function bootstrap() {
   const logger = new Logger('Main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    },
+  );
 
-  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -18,8 +27,7 @@ async function bootstrap() {
     })
   );
 
-  await app.listen(envs.port);
-  logger.log(`Aplicación ejecutandose en el puerto ${envs.port}`);
+  logger.log(`Aplicación de microservicio ejecutandose en el puerto ${envs.port}`);
   
 }
 bootstrap();
--- 02-products-app/products-ms/src/products/products.controller.ts
+++ 02-products-app/products-ms/src/products/products.controller.ts
@@ -1,35 +1,44 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from './common/dto';
 
+
 @Controller('/products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private readonly productsService: ProductsService) { }
 
-  @Post('/create')
-  create(@Body() createProductDto: CreateProductDto) {
+  // @Post('/create')
+  @MessagePattern({ cmd: 'create_product' })
+  create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @Get('/all')
-  findAll(@Query() paginationDto: PaginationDto) {
+
+  // @Get('/all')
+  @MessagePattern({ cmd: 'all_products' })
+  findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @Get('/:id')
-  findOne(@Param('id') id: string) {
-    return this.productsService.findOne(+id);
+  // @Get('/:id')
+  @MessagePattern({ cmd: 'one_product' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
   }
 
-  @Patch('/update/:id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+  // @Patch('/update/:id')
+  @MessagePattern({ cmd: 'update_product' })
+  update(@Payload() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id', ParseIntPipe) id: number) {
+  // @Delete('/delete/:id')
+  @MessagePattern({ cmd: 'delete_product' })
+  remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 }
