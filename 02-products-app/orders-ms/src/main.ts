ce17609fc8b7ca488abfe7eabdd713cd32becdd3
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-28 12:25:03 -0500

    Creación de microservicio (No HTTP) desde la consola

--- 02-products-app/orders-ms/src/main.ts
+++ 02-products-app/orders-ms/src/main.ts
@@ -1,8 +1,25 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+
+
 import { AppModule } from './app.module';
+import { envs } from './config';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Orders-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, 
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    }
+  );
+ 
+
+  logger.log(`Microservicio de orders ejecutando en el puerto ${envs.port}`);
 }
 bootstrap();
