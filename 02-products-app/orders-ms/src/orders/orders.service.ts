636dfe384392e0c946282306aea48290e175ccca
Franz Flores <andresfloresgallardo@hotmail.com>
2024-08-30 12:35:32 -0500

    Conexión con BD Postgres y Prisma en microservicio de orders

--- 02-products-app/orders-ms/src/orders/orders.service.ts
+++ 02-products-app/orders-ms/src/orders/orders.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Injectable()
-export class OrdersService {
+export class OrdersService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('OrderService');
+
+  async onModuleInit() {
+    await this.$connect();
+    this.logger.log('Base de datos conectada');
+  }
+
   create(createOrderDto: CreateOrderDto) {
     return 'This action adds a new order';
   }
