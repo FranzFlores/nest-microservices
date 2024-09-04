429e10c962d451940e70531afca17f65be958f42
Franz Flores <andresfloresgallardo@hotmail.com>
2024-09-04 17:29:56 -0500

    Cambio de TCP a Nats en modulo de productos en gateway

--- 02-products-app/client-gateway/src/products/products.controller.ts
+++ 02-products-app/client-gateway/src/products/products.controller.ts
@@ -11,12 +11,12 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductsController {
   constructor(
     @Inject(PRODUCT_SERVICE)
-    private readonly productsClient: ClientProxy
+    private readonly client: ClientProxy
   ) { }
 
   @Post('/create')
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient.send({ cmd: 'create_product' }, createProductDto)
+    return this.client.send({ cmd: 'create_product' }, createProductDto)
       .pipe(
         catchError(err => { throw new RpcException(err) })
       );
@@ -24,7 +24,7 @@ export class ProductsController {
 
   @Get('/all')
   findAllProducts(@Query() paginationDto: PaginationDto) {
-    return this.productsClient.send({ cmd: 'all_products' }, paginationDto)
+    return this.client.send({ cmd: 'all_products' }, paginationDto)
       .pipe(
         catchError(err => { throw new RpcException(err) })
       );
@@ -32,7 +32,7 @@ export class ProductsController {
 
   @Get('/:id')
   async findOneProducts(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'one_product' }, { id })
+    return this.client.send({ cmd: 'one_product' }, { id })
       .pipe(
         catchError(err => { throw new RpcException(err) })
       );
@@ -40,7 +40,7 @@ export class ProductsController {
 
   @Patch('/update/:id')
   updateProduct(@Param('id', ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsClient.send({ cmd: 'update_product' }, { id, ...updateProductDto })
+    return this.client.send({ cmd: 'update_product' }, { id, ...updateProductDto })
       .pipe(
         catchError(err => { throw new RpcException(err) })
       );
@@ -48,7 +48,7 @@ export class ProductsController {
 
   @Delete('/delete/:id')
   deleteProduct(@Param('id', ParseIntPipe) id: number) {
-    return this.productsClient.send({ cmd: 'delete_product' }, { id })
+    return this.client.send({ cmd: 'delete_product' }, { id })
       .pipe(
         catchError(err => { throw new RpcException(err) })
       );
--- 02-products-app/client-gateway/src/products/products.module.ts
+++ 02-products-app/client-gateway/src/products/products.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { ProductsController } from './products.controller';
-import { envs, PRODUCT_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: PRODUCT_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          host: envs.productsMicroserviceHost,
-          port: envs.productsMicroservicePort
-        }
-      },
-    ]),
+    NatsModule
   ],
   controllers: [ProductsController],
   providers: [],
