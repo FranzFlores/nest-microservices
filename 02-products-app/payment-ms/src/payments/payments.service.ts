99d4967a0e55fec4e59d6b148676d56d34809bbe
Franz Flores <andresfloresgallardo@hotmail.com>
2024-09-21 13:07:07 -0500

    Creación de DTO para el servicio de pago con Stripe

--- 02-products-app/payment-ms/src/payments/payments.service.ts
+++ 02-products-app/payment-ms/src/payments/payments.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
-
 import Stripe from 'stripe';
+
+import { envs } from 'src/config';
+import { PaymentSessionDto } from './dto/payment-session.dto';
 
 @Injectable()
 export class PaymentsService {
-    private readonly strip = new Stripe(envs.stripeSecret);
+    private readonly stripe = new Stripe(envs.stripeSecret);
+
+    async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+        const {currency, items} = paymentSessionDto;
+
+        const lineItems = items.map(item => {
+            return {
+                price_data: {
+                    currency: currency,
+                    product_data: {
+                        name: item.name
+                    },
+                    unit_amount: Math.round(item.price * 100), // 20 dolares
+                },
+                quantity: item.quantity // Cantidad del producto
+            }
+        });
+        
+        const session = await this.stripe.checkout.sessions.create({
+            // Colocar el id de la orden
+            payment_intent_data: {
+                metadata: {}
+            },
+            line_items: lineItems,
+            mode: 'payment',
+            success_url: 'http://localhost:3000/payments/success',
+            cancel_url: 'http://localhost:3000/payments/cancel'
+        });
+
+        return session;
+    }
 }
