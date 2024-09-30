ed2d18de0fb2632831f996a5739971b0939a6118
Franz Flores <andresfloresgallardo@hotmail.com>
2024-09-30 14:35:35 -0500

    Elaboración de Dtos para auth

--- 02-products-app/auth-ms/src/main.ts
+++ 02-products-app/auth-ms/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
@@ -11,6 +12,13 @@ async function bootstrap() {
       servers: envs.natsServers
     }
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  );
 
 
   await app.listen();
