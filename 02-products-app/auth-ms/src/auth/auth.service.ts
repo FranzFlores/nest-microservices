3440a29d66b6cbfe185eea3705296fff774c503a
Franz Flores <andresfloresgallardo@hotmail.com>
2024-09-30 19:45:40 -0500

    Recurso para crear registro de usuario

--- 02-products-app/auth-ms/src/auth/auth.service.ts
+++ 02-products-app/auth-ms/src/auth/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+import { RegisterUserDto } from './dto';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -8,6 +12,42 @@ export class AuthService extends PrismaClient implements OnModuleInit {
     onModuleInit() {
         this.$connect();
         this.logger.log('MongoDB Conectado');
+    }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+        const { email, name, password } = registerUserDto;
+        try {
+            const user = await this.user.findUnique({
+                where: {
+                    email: email
+                }
+            });
+
+            if (user) {
+                throw new RpcException({
+                    status: 400,
+                    message: 'El usuario ya existe'
+                });
+            }
+
+            const newUser = await this.user.create({
+                data: {
+                    email: email,
+                    name: name,
+                    password: bcrypt.hashSync(password, 10)
+                }
+            });
+
+            const { password: __, ...rest } = newUser;
+
+            return { user: rest }
+        } catch (error) {
+            console.log(error);
+            throw new RpcException({
+                status: 400,
+                message: error.message
+            });
+        }
     }
 
 }
